7062a0f6163a02db44b436c9abec3ea32645c8b0
prathap <prathap.mohan@kloctechnologies.co.in>
2019-05-11 11:30:17 +0530

    added user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
+# frozen_string_literal: true
 
 RSpec.describe User, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'validates email presence' do 
+      is_expected.to validate_presence_of(:email)
+    end
+    it 'validates email Uniqueness' do 
+      u1 = User.create(email: 'p1@gmail.com', password: '1234567', password_confirmation: '1234567')
+      u2 = User.create(email: 'p1@gmail.com', password: '1234567', password_confirmation: '1234567')
+      expect(u2.errors.full_messages).to include('Email has already been taken')
+    end
+    it 'validates password length' do 
+      u1 = User.create( email: 'p1@gmail.com', password: '12', password_confirmation: '1234567')
+      expect(u1.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+    end
+  end
 end
