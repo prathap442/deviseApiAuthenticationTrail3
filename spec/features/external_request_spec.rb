aca4eaa21cef5597fd814bfa17651714ec92bca2
prathap <prathap.mohan@kloctechnologies.co.in>
2019-05-11 18:32:38 +0530

    failed: trail1

--- spec/features/external_request_spec.rb
+++ spec/features/external_request_spec.rb
@@ -1,10 +1,12 @@
 # spec/features/external_request_spec.rb
 require 'spec_helper'
+require 'vcr'
 require 'httparty'
 RSpec.describe 'External request' do
+  before(:example) { HTTParty.get('https://klocgold.myshopify.com/products').stub }
   it 'queries no. of products on the klocgold' do
-    uri = URI('https://api.github.com/repos/thoughtbot/factory_girl/contributors')
-    response = Net::HTTP.get(uri)
-    expect(response.class).to be(String)
+    VCR.use_cassette('klocgold_products') do
+      expect(response_obtained.body.class).to be(String)
+    end
   end
 end
